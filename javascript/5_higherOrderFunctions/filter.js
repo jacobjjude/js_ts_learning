0fd186b4ae323e9bbbb172199b4439f322cd1d54
Jacob <jacobj.jude@outlook.com>
2024-07-06 18:03:30 -0400

    beginning chapter 5

---
+++ javascript/5_higherOrderFunctions/filter.js
@@ -0,0 +1,13 @@
+const SCRIPTS = require('./scripts');
+
+function filter(array, test) {
+    let passed = [];
+    for (let element of array) {
+        if (test(element)) {
+            passed.push(element);
+        }
+    }
+    return passed;
+}
+
+console.log(filter(SCRIPTS, script => script.living));
