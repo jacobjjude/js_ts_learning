9b25c3e367e9a2f2e83bd3eeba4ed7a239f74d5d
Jacob <jacobj.jude@outlook.com>
2024-07-15 01:39:06 -0400

    flatten arrays

---
+++ javascript/5_higherOrderFunctions/everything.js
@@ -0,0 +1,7 @@
+const arrayOfArrays = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"]
+]
+
+console.log(arrayOfArrays.reduce((a, b) => a.concat(b)));
--- javascript/5_higherOrderFunctions/filter.js
+++ javascript/5_higherOrderFunctions/filter.js
@@ -18,6 +18,39 @@ function map(array, transform) {
     return mapped;
 }
 
+function reduce(array, combine, start) {
+    let  current = start;
+    for (let element of array) {
+        current = combine(current, element);
+    }
+    return current;
+}
+
+function characterCount(script) {
+    return script.ranges.reduce((count, [from, to]) => {
+        return count + (to - from);
+    }, 0);
+}
+
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => { return code >= from && code < to; })) {
+            return script;
+        }
+    }
+    return null;
+}
+
+
 console.log(filter(SCRIPTS, script => script.living));
 let rtlscripts = SCRIPTS.filter(s => s.direction == "rtl");
 console.log(map(rtlscripts, s => s.name));
+
+console.log([1,2,3,4].reduce((a , b) => a + b, 0))
+
+console.log(SCRIPTS.reduce((a, b) => {
+    return characterCount(a) < characterCount(b) ? b : a;
+}));
+
+console.log(characterScript(121));
