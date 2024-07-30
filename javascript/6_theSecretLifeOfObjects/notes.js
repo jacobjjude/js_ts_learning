4e702a570ef40135a4f0286b5461b2e79f7dc24d
Jacob <jacobj.jude@outlook.com>
2024-07-30 16:53:50 -0400

    073024

--- javascript/6_theSecretLifeOfObjects/notes.js
+++ javascript/6_theSecretLifeOfObjects/notes.js
@@ -14,13 +14,13 @@
 // Methods are nothing more than properties that hold function values
 
 function speak(line) {
-    console.log(`The ${this.type} rabbit says '${line}'`)
+  console.log(`The ${this.type} rabbit says '${line}'`);
 }
 
-let whiteRabbit = {type: "white", speak};
-let hungryRabbit = {type: "hungry", speak};
+let whiteRabbit = { type: "white", speak };
+let hungryRabbit = { type: "hungry", speak };
 
-console.log("Methods:")
+console.log("Methods:");
 whiteRabbit.speak("Oh my fur and whiskers");
 hungryRabbit.speak("Got any carrots?");
 
@@ -32,19 +32,20 @@ speak.call(whiteRabbit, "Hurry");
 // Arrow functions do not bind a this but can see the "this" of the scope around them
 
 let finder = {
-    find(array) {
-        return array.some(v => v == this.value);
-    }, value: 5
+  find(array) {
+    return array.some((v) => v == this.value);
+  },
+  value: 5,
 };
 console.log("Finder w/ arrow function: ");
-console.log(finder.find([4,5]));
+console.log(finder.find([4, 5]));
 
 //*Prototypes*
 //- Prototypes: an object that defines a set of default properties
 // Use object.create to create an object with a specific prototype
 newSection();
 
-console.log("Prototypes: ")
+console.log("Prototypes: ");
 let empty = {};
 console.log(empty.toString);
 console.log(empty.toString());
@@ -61,9 +62,9 @@ console.log(Object.getPrototypeOf([]) == Array.prototype);
 newLine();
 
 let protoRabbit = {
-    speak(line) {
-        console.log(`The ${this.type} rabbit says '${line}'`);
-    }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  },
 };
 let blackRabbit = Object.create(protoRabbit);
 blackRabbit.type = "black";
@@ -77,42 +78,126 @@ newSection();
 
 console.log("Classes: ");
 function makeRabbit(type) {
-    let rabbit = Object.create(protoRabbit);
-    rabbit.type = type;
-    return rabbit;
+  let rabbit = Object.create(protoRabbit);
+  rabbit.type = type;
+  return rabbit;
 }
 
 // - class keyword starts a class declaration, which allows us to define a constructor and a set of methods together
 class Rabbit {
-    constructor(type) {
-        this.type = type;
-    }
-    speak(line) {
-        console.log(`The ${this.type} rabbit says '${line}'`);
-    }
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
 }
 
 let killerRabbit = new Rabbit("killer");
 function ArchaicRabbit(type) {
-    this.type = type;
+  this.type = type;
 }
-ArchaicRabbit.prototype.speak = function(line) {
-    console.log(`The ${this.type} rabbit says '${line}'`);
-    };
+ArchaicRabbit.prototype.speak = function (line) {
+  console.log(`The ${this.type} rabbit says '${line}'`);
+};
 let oldSchoolRabbit = new ArchaicRabbit("old school");
 
-console.log(Object.getPrototypeOf(Rabbit) ==
-Function.prototype);
-console.log(Object.getPrototypeOf(killerRabbit) ==
-Rabbit.prototype);
+console.log(Object.getPrototypeOf(Rabbit) == Function.prototype);
+console.log(Object.getPrototypeOf(killerRabbit) == Rabbit.prototype);
 
-let object = new class { getWord() { return "hello"; } };
+let object = new (class {
+  getWord() {
+    return "hello";
+  }
+})();
+newSection();
+
+//Private Properties:
+// - Private Properties: properties and methods for internal use that are not part of the interface.
+// -- to declare a private property, put a # in front of its name
+// -- These methods can only be called inside the class declaration that defines them
+// Private properties MUST be declared in the class declaration to be available at all
+class SecretiveObject {
+  #getSecret() {
+    return "I ate all the plums";
+  }
+  interrogate() {
+    let shallISayIt = this.#getSecret();
+    return "never";
+  }
+}
+
+class RandomSource {
+  #max;
+  constructor(max) {
+    this.#max = max;
+  }
+  getNumber() {
+    return Math.floor(Math.random() * this.#max);
+  }
+}
+newSection();
+
+//*Overriding Derived Properties*
+// - When you add a property to an object, the property is added to the object itself
+// - If there was already a property with the same name in the prototype, this property will no longer affect the object, its hidden behind the object's own property.
+
+console.log("Overriding Dereived Properties");
+Rabbit.prototype.teeth = 'small';
+console.log(killerRabbit.teeth);
+killerRabbit.teeth = "long, sharp, and bloody";
+console.log(killerRabbit.teeth);
+console.log((new Rabbit("basic")).teeth);
+console.log(Rabbit.prototype.teeth);
+
+// - Overriding can be used to express exceptional properties in instances of a more generic class of objects while letting the nonexceptional objects take a standard value from their prototype
+
+newSection();
+
+// **MAPS**
+// - MAP (noun) data structure that associates value (the keys) with other values.
+// - For example, you might want to map names to ages
+
+console.log("MAPS")
+let ages = {
+  Boris: 39,
+  Liang: 22,
+  Julia: 62
+};
+
+console.log(`Julia is ${ages['Julia']}`);
+console.log("Is Jack's age known?", "Jack" in ages);
+console.log("Is toString's age known?", "toString" in ages);
+
+// - Object's property names are the people's names and the property values are their ages.
+// - Using plain objects as maps is dangerous. There are several ways to avoid this problem
+// - - 1) create objects with NO prototype. Pass null to object.create, the object will not derive from Object.prototype and can be used safely as map
+
+console.log("toString" in Object.create(null));
+
+// - - Object property names must be strings. If you need a map who's keys can't easily be converted to strings - such as objects - you cannot use an object as your map
+// JavaScript comes with a class called Map that is written for this purpose. It stores a mapping and allows any type of keys
+
+let ages_2 = new Map();
+ages_2.set("Boris", 39);
+ages_2.set("Liang", 22);
+ages_2.set("Julia", 62);
+console.log(`Júlia is ${ages_2.get("Júlia")}`);
+console.log("Is Jack's age known?", ages_2.has("Jack"));
+console.log(ages_2.has("toString"));
+
+// - The methods set, get, and has are part of the interface of the Map object. 
+// - If you have a plain object that you need to treat as a map, Object.keys returns only an object's OWN keys, not those in the prototype.
+// - As an alternative, you can use the Object.hasOwn function, which ignore's the objects prototype.
+
+console.log(Object.hasOwn({x: 1}, "x"));
+console.log(Object.hasOwn({x: 1}, "toString"));
 
 //Making my life easier
 function newSection() {
-    return console.log("------------------------\n \n");
+  return console.log("------------------------\n \n");
 }
 
 function newLine() {
-    return console.log("\n");
+  return console.log("\n");
 }
