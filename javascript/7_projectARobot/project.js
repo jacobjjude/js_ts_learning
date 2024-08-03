518c54cee608ce707b5fcc52bce0591c230c0c2f
Jacob <jacobj.jude@outlook.com>
2024-08-03 04:44:25 -0400

    exercise 1 done

--- javascript/7_projectARobot/project.js
+++ javascript/7_projectARobot/project.js
@@ -65,12 +65,13 @@ function runRobot(state, robot, memory) {
     for (let turn=0;; turn++) {
         if(state.parcels.length == 0) {
             console.log(`Done in ${turn} turns`);
+            return turn;
             break;
         }
         let action = robot(state, memory);
         state = state.move(action.direction);
         memory = action.memory;
-        console.log(`Moved to ${action.direction}`);
+        // console.log(`Moved to ${action.direction}`);
     }
 }
 
@@ -116,7 +117,7 @@ function routeRobot(state, memory) {
     return {direction: memory[0], memory: memory.slice(1)};
 }
 
-// runRobot(VillageState.random(), routeRobot, mailRoute);
+runRobot(VillageState.random(), routeRobot, mailRoute);
 
 function findRoute(graph, from, to) {
     let work = [{at: from, route: []}];
@@ -143,4 +144,34 @@ function goalOrientedRobot ({place, parcels}, route) {
     return {direction: route[0], memory: route.slice(1)};
 }
 
-// runRobot(VillageState.random(), goalOrientedRobot, mailRoute);
+let task = VillageState.random();
+console.log(task);
+
+// Exercise 1:
+// Write a function that compares two robots
+// Should generate 100 tasks and let each robot solve each of the tasks. 
+// Output the average number of steps each robot took per task
+// Same tasks per robot
+
+let goalBot = runRobot(VillageState.random(), goalOrientedRobot, mailRoute);
+let randoBot = runRobot(VillageState.random(), randomRobot);
+console.log(goalBot);
+console.log(randoBot);
+
+function compareRobots(robot1, robot2, tasktotal) {
+    let total1 = 0;
+    let total2 = 0;
+
+    for (let i = 0; i < tasktotal; i++) {
+        let task = VillageState.random();
+        let result1 = runRobot(task, robot1, mailRoute);
+        let result2 = runRobot(task, robot2, mailRoute);
+
+        total1 += result1;
+        total2 += result2
+    }
+    console.log(total1 / tasktotal);
+    console.log(total2 / tasktotal);
+}
+
+compareRobots(randomRobot, goalOrientedRobot, 100);
